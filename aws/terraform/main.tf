1b41c371690afef51a5115c180dc589396233788
Lyonga <60431189+Lyonga@users.noreply.github.com>
2023-07-09 13:45:54 -0400

    Update main.tf

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -135,10 +135,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
-  statement {
-    actions   = ["ecr:*"]
-    resources = ["*"]
-  }
 }
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
