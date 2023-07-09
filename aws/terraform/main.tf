5cb8d92c88e94f551ba9ae130f6ef370e3eeda9c
Lyonga <60431189+Lyonga@users.noreply.github.com>
2023-07-09 13:37:38 -0400

    Update main.tf

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -124,9 +124,6 @@ resource "aws_lb_listener" "listener" {
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "testecsTaskExecutionRole"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-  lifecycle {
-    ignore_changes = all
-  }
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -174,14 +171,3 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
-resource "aws_iam_policy" "ecsTaskExecutionPolicy" {
-  name   = "ecsTaskExecutionPolicy"
-  policy = data.aws_iam_policy_document.assume_role_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_custom_policy_attachment" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = aws_iam_policy.ecsTaskExecutionPolicy.arn
-}
-
